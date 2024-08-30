89c2206a0dc2f7101ec6a33d85a85a98a69d573a
Fulvia Mourikis <zum281@gmail.com>
2024-08-30 17:33:00 +0200

    feat: final touches

--- main.js
+++ main.js
@@ -25,10 +25,25 @@ const playRound = (
   const roundContainer = document.querySelector("#round");
   const endGameContainer = document.querySelector(".game-over");
   const endGameMessage = document.querySelector("#game-over-message");
+  const modal = document.querySelector("#modal");
 
   const score = checkWinner(playerChoice);
   playerScore += score[0];
   aiScore += score[1];
+
+  modal.innerHTML = `
+    <div class="modal-content">
+      <p>${
+        score[0] > score[1]
+          ? "Player wins"
+          : score[0] < score[1]
+          ? "AI wins"
+          : "It's a tie"
+      }</p>
+      <button id="restart" onclick="newRound()">New Round</button>
+    </div>
+  `;
+  modal.style.display = "flex";
 
   playerScoreCointainer.innerHTML = playerScore;
   aiScoreContainer.innerHTML = aiScore;
@@ -47,15 +62,20 @@ const playRound = (
     const winner =
       playerScore > aiScore ? "player" : playerScore < aiScore ? "ai" : "tie";
 
-    if (winner !== "tie") endGameMessage.innerHTML = `${winner} wins the game!`;
-    else endGameMessage.innerHTML = "It's a tie!";
+    if (winner !== "tie") {
+      endGameMessage.innerHTML = `${winner} wins the game!`;
+    } else {
+      endGameMessage.innerHTML = "It's a tie!";
+    }
   }
 
   return { playerScore, aiScore, round, endGame };
 };
 
 const playGame = () => {
-  const playerButtons = document.querySelectorAll("button");
+  const playerButtons = document.querySelectorAll(
+    "button:not(#restart):not(#modal button):not(#end-game button)",
+  );
 
   let playerScore = 0;
   let aiScore = 0;
@@ -65,6 +85,7 @@ const playGame = () => {
   playerButtons.forEach((button) =>
     button.addEventListener("click", (e) => {
       const playerChoice = e.target.alt;
+
       const result = playRound(
         playerChoice,
         playerScore,
@@ -80,6 +101,12 @@ const playGame = () => {
       endGame = result.endGame;
     }),
   );
+};
+
+const newRound = () => {
+  const modal = document.querySelector("#modal");
+  modal.innerHTML = "";
+  modal.style.display = "none";
 };
 
 playGame();
